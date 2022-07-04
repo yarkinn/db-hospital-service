5995294bbd73944b37e3ea9d54ef9cd1f8359fb0
yarkinn <82332307+yarkinn@users.noreply.github.com>
2022-07-04 16:37:21 +0300

    Initial commit

---
+++ src/main/java/com/example/dbhospitalservice/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.dbhospitalservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/entity/EmployeeEntity.java
@@ -0,0 +1,77 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee", schema = "hospital", catalog = "")
+public class EmployeeEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "salary")
+    private Integer salary;
+    @Basic
+    @Column(name = "hours")
+    private Integer hours;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EmployeeEntity that = (EmployeeEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (salary != null ? !salary.equals(that.salary) : that.salary != null) return false;
+        if (hours != null ? !hours.equals(that.hours) : that.hours != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (salary != null ? salary.hashCode() : 0);
+        result = 31 * result + (hours != null ? hours.hashCode() : 0);
+        return result;
+    }
+}
